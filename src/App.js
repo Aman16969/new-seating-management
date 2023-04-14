ddc92b8fe5f1676e5697872c4353a0935993f9d5
Dency631 <dency.patel@accolitedigital.com>
2023-04-14 21:56:39 +0530

    Routing of location

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import PrivateRoutes from "./PrivateRoutes";
 import "./App.css";
 import Navbar from "./Component/NavBar/Navbar";
 import Home from "./Component/Home/Home";
-import { Auth} from "../src/ContextApi/AuthContext";
+import { Auth } from "../src/ContextApi/AuthContext";
+import Location from "./Component/Location/Location";
 function App() {
   return (
     <div className="App">
       <Auth>
-      <Navbar/>
-      <Router>
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route path="/home" element={<Home />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/location" element={<Location />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
       </Auth>
     </div>
   );
