d589aa3f29c272c447d61097fdf15005d9c333e1
Dency631 <dency.patel@accolitedigital.com>
2023-04-24 17:16:53 +0530

    location add done

--- src/Component/Location/Location.js
+++ src/Component/Location/Location.js
@@ -7,6 +7,7 @@ const Location = () => {
   const [locations, setLocations] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(null);
   const token = sessionStorage.getItem("accessToken");
 
   useEffect((e) => {
@@ -38,6 +39,38 @@ const Location = () => {
     setSelectedLocation(selectedLocation);
   };
 
+  const addLocation = (event) => {
+    event.preventDefault();
+    const name = prompt("Enter location name:");
+    if (!name) return;
+    const location = { name };
+    setIsPending(true);
+    console.log(location);
+    fetch("http://localhost:8081/api/location/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(location),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setLocations([...locations, data]);
+        setSelectedLocation(data);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  };
+
   return (
     <>
       <div className="container-content">
@@ -45,21 +78,25 @@ const Location = () => {
           <div class="row-card-title">
             <h2>Locations</h2>
           </div>
-          <select
-            className="drop-select"
-            name="select"
-            id="select"
-            value={selectedLocation?.id || ""}
-            onChange={handleLocationChange}
-          >
-            <option value="">Select a location</option> // add a default option
-            with an empty value
-            {locations.map((location) => (
-              <option key={location.id} value={location.id}>
-                {location.name}
-              </option>
-            ))}
-          </select>
+          <div className="locationList">
+            <select
+              name="select"
+              id="select"
+              value={selectedLocation?.id || ""}
+              onChange={handleLocationChange}
+            >
+              <option value="">Select a location</option> // add a default
+              option with an empty value
+              {locations.map((location) => (
+                <option key={location.id} value={location.id}>
+                  {location.name}
+                </option>
+              ))}
+            </select>
+            <button onClick={addLocation} disabled={isPending}>
+              Add Location
+            </button>
+          </div>
           {selectedLocation && <LocationLayout location={selectedLocation} />}{" "}
         </div>
       </div>
