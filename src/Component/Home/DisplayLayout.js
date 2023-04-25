4326f8d215a6c047b20b56d799a366f5926559c3
Dency631 <dency.patel@accolitedigital.com>
2023-04-25 13:39:30 +0530

    reloading done

--- src/Component/Home/DisplayLayout.js
+++ src/Component/Home/DisplayLayout.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import DisplaySeat from "./DisplaySeat";
 
-const DisplayLayout = ({ location, seatAvailability, seats, date, fromTime, toTime }) => {
+const DisplayLayout = ({
+  location,
+  seatAvailability,
+  seats,
+  date,
+  fromTime,
+  toTime,
+}) => {
   const [error, setError] = useState(null);
   const token = sessionStorage.getItem("accessToken");
   const [selected, setSelected] = useState(null);
@@ -20,23 +27,29 @@ const DisplayLayout = ({ location, seatAvailability, seats, date, fromTime, toTi
     };
     fetch(`http://localhost:8081/api/booking/`, {
       method: "POST",
-      headers: { "content-type": "application/json", Authorization: "Bearer "+token },
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + token,
+      },
       body: JSON.stringify(bookingDetail),
-    }).then((res) => {
-      if (!res.ok) {
-        throw Error("failed to book seat");
-      }
-      setSelected(null);
-      return res.json();
     })
-    .then((data) => {
-      console.log(data);
-      setStatus(data.isSuccessful);
-      setMessage(data.message);
-    }).catch((error)=>{
-      setError(error.message);
-      console.log(error.message);
-    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("failed to book seat");
+        }
+        setSelected(null);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setStatus(data.isSuccessful);
+        setMessage(data.message);
+        window.location.reload();
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.log(error.message);
+      });
   };
 
   const rows = [];
@@ -44,39 +57,44 @@ const DisplayLayout = ({ location, seatAvailability, seats, date, fromTime, toTi
     const cols = [];
     for (let j = 1; j <= location.cs; j++) {
       const id = location.id + "R" + i + "C" + j;
-      if(seatAvailability===null){
-      cols.push(
-        <td>
-          <div id={id} name={id}>
-            {
-              <DisplaySeat
-                location={location}
-                row={i}
-                col={j}
-                status={false}
-              ></DisplaySeat>
-            }
-          </div>
-        </td>
-      );
-    }
-    else{
-      cols.push(
-        <td>
-          <div id={id} name={id} onClick={()=>{setSelected(id)}}>
-            {
-              <DisplaySeat
-                location={location}
-                row={i}
-                col={j}
-                selected={selected}
-                status={seatAvailability.hasOwnProperty(id)}
-              ></DisplaySeat>
-            }
-          </div>
-        </td>
-      );
-    }
+      if (seatAvailability === null) {
+        cols.push(
+          <td>
+            <div id={id} name={id}>
+              {
+                <DisplaySeat
+                  location={location}
+                  row={i}
+                  col={j}
+                  status={false}
+                ></DisplaySeat>
+              }
+            </div>
+          </td>
+        );
+      } else {
+        cols.push(
+          <td>
+            <div
+              id={id}
+              name={id}
+              onClick={() => {
+                setSelected(id);
+              }}
+            >
+              {
+                <DisplaySeat
+                  location={location}
+                  row={i}
+                  col={j}
+                  selected={selected}
+                  status={seatAvailability.hasOwnProperty(id)}
+                ></DisplaySeat>
+              }
+            </div>
+          </td>
+        );
+      }
     }
     rows.push(<tr>{cols}</tr>);
   }
@@ -85,9 +103,17 @@ const DisplayLayout = ({ location, seatAvailability, seats, date, fromTime, toTi
     <>
       <h1>{location.name}</h1>
       <table className="locationTable">{rows}</table>
-      {selected && <button onClick={()=>{handleBooking()}}>Book Seat</button>}
-      {message && status===0 && <h3 style={{color:"red"}}>{message}</h3>}
-      {message && status===1 && <h3 style={{color:"green"}}>{message}</h3>}
+      {selected && (
+        <button
+          onClick={() => {
+            handleBooking();
+          }}
+        >
+          Book Seat
+        </button>
+      )}
+      {message && status === 0 && <h3 style={{ color: "red" }}>{message}</h3>}
+      {message && status === 1 && <h3 style={{ color: "green" }}>{message}</h3>}
     </>
   );
 };
