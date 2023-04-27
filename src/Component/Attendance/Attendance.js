0714a739d6fa37ff5821a1d1bfe277bad4c30f19
Dency631 <dency.patel@accolitedigital.com>
2023-04-27 14:31:52 +0530

    Attendance sheet fixed

--- src/Component/Attendance/Attendance.js
+++ src/Component/Attendance/Attendance.js
@@ -1,49 +1,58 @@
-import { useRef } from "react";
+import { useState } from "react";
+import * as XLSX from "xlsx";
 import "./Attendance.css";
 import { AttendanceExcel } from "./AttendanceExcel";
 
 const Attendance = () => {
-  const fileInput = useRef(null);
-  const filePathset = (e) => {
-    console.log(e.target.files[0].name);
-  };
+  const [jsonData, setJsonData] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    AttendanceExcel(fileInput.current.files[0]);
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const json = XLSX.utils.sheet_to_json(worksheet, {
+        raw: false,
+        dateNF: "mm/dd/yyyy",
+        header: ["Emp_ID", "Name", "Date", "In_Time", "Out_Time"],
+      });
+
+      const formattedJson = json.map((item) => {
+        return {
+          ...item,
+          Date: item.Date.split("/").reverse().join("-"),
+        };
+      });
+
+      setJsonData(formattedJson);
+    };
+    reader.readAsBinaryString(file);
   };
 
   return (
-    <>
-      <div className="container">
-        <div className="container-content">
-          <div className="row-card" style={{ height: "86.5vh" }}>
-            Upload the Excel Attendance Sheet
-            <div>
-              <form className="attendance-form">
-                <label for="date"> Select Date </label>
-                <input
-                  type="date"
-                  name="date"
-                  id="date"
-                  min={new Date().toISOString().split("T")[0]} // Set minimum date to today
-                />
-
-                <label htmlFor="attendance"> Upload the Excel Sheet</label>
-                <input
-                  type="file"
-                  id="file"
-                  ref={fileInput}
-                  name="file"
-                  onChange={filePathset}
-                />
-                <button onClick={handleSubmit}>Fetch attendance</button>
-              </form>
-            </div>
+    <div className="container">
+      <div className="container-content">
+        <div className="row-card" style={{ height: "86.5vh" }}>
+          <h2>Upload the Excel Attendance Sheet</h2>
+          <div>
+            <form className="attendance-form">
+              <label for="date">Select Date</label>{" "}
+              <input
+                type="date"
+                name="date"
+                id="date"
+                min={new Date().toISOString().split("T")[0]}
+              />
+              <label htmlFor="attendance">Upload the Excel Sheet</label>
+              <input type="file" onChange={handleFile} />
+              {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>}
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Component/Attendance/AttendanceExcel.js
+++ src/Component/Attendance/AttendanceExcel.js
@@ -8,17 +8,18 @@ export const AttendanceExcel = (file) => {
     const workbook = XLSX.read(data, { type: "binary" });
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
     const attendanceData = XLSX.utils
-      .sheet_to_json(worksheet, {
-        header: ["Emp_ID", "Name", "Date", "In_Time", "Out_Time"],
-        dateNF: "mm/dd/yyyy",
-        timeNF: "hh:mm:ss AM/PM",
-      })
-      .map((row) => ({
-        ...row,
-        Date: row.Date ? new Date(row.Date) : null,
-        In_Time: row.In_Time ? new Date(row.In_Time) : null,
-        Out_Time: row.Out_Time ? new Date(row.Out_Time) : null,
-      }));
+  .sheet_to_json(worksheet, {
+    header: ["Emp_ID", "Name", "Date", "In_Time", "Out_Time"],
+    dateNF: "yyyy/mm/dd",
+    timeNF: "hh:mm:ss AM/PM",
+  })
+  .map((row) => ({
+    ...row,
+    Date: typeof row.Date === "string" ? row.Date.replace(/\//g, "-") : null,
+    In_Time: row.In_Time ? new Date(row.In_Time) : null,
+    Out_Time: row.Out_Time ? new Date(row.Out_Time) : null,
+  }));
+
     console.log(attendanceData);
   };
   reader.readAsBinaryString(file);
