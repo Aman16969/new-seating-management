edb449e4bb029f7d24cc00aa3dd3c573dd9b0195
Rajavarapu Visswateza <rajavarapu-visswateza@gmail.com>
2023-04-27 15:17:07 +0530

    admin stats

--- src/Component/Admin/AdminStats.js
+++ src/Component/Admin/AdminStats.js
@@ -1,9 +1,10 @@
-import jsPDF from "jspdf";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import FileSaver from "file-saver";
 
-function AdminStats () {
-  const [fromDate, setFromDate]= useState(null);
-  const [toDate, setToDate]= useState(null);
+function AdminStats() {
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleFromDateChange = (event) => {
     setFromDate(event.target.value);
@@ -12,24 +13,58 @@ function AdminStats () {
     setToDate(event.target.value);
   };
   const handleDownloadPdf = () => {
-    const doc = new jsPDF();
-    doc.text(`From Date: ${fromDate}`, 10, 10);
-    doc.text(`To Date: ${toDate}`,10,20);
-    doc.save('statistics.pdf');
+    const header = "Bearer " + sessionStorage.getItem("accessToken");
+    // const locationId = sessionStorage.getItem("userLocationId");
+    fetch(
+      `http://localhost:8081/api/pdf/bookings/dateWise/${fromDate}/${toDate}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: header,
+        },
+      }
+    )
+      .then((response) => {
+        return response.blob();
+      })
+      .then((blob) => {
+        // Use FileSaver.js to save the blob as a file
+        FileSaver.saveAs(blob, `Bookings between ${fromDate} and ${toDate}.pdf`);
+      })
+      .catch((error) => {
+        console.error("Error downloading PDF:", error);
+      });
   };
   return (
     <>
       <div>
         <label htmlFor="fromDate">From Date:</label>
-        <input type="date" id="fromDate" value={fromDate} onChange={handleFromDateChange} /><br></br>
+        <input
+          type="date"
+          id="fromDate"
+          value={fromDate}
+          onChange={handleFromDateChange}
+        />
+        <br></br>
 
-     <label htmlFor="toDate">To Date:</label>
-        <input type="date" id="toDate" value={toDate} onChange={handleToDateChange} /><br></br>
+        <label htmlFor="toDate">To Date:</label>
+        <input
+          type="date"
+          id="toDate"
+          value={toDate}
+          onChange={handleToDateChange}
+        />
+        <br></br>
 
-       <center> <button className="ad" onClick={handleDownloadPdf}>Download PDF</button></center>
+        <center>
+          {" "}
+          <button className="ad" onClick={handleDownloadPdf}>
+            Download PDF
+          </button>
+        </center>
       </div>
     </>
   );
-};
+}
 
 export default AdminStats;
