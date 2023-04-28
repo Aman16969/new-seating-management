25ef3fab105e4594e17123cc79d6565525ee307f
Rajavarapu Visswateza <rajavarapu-visswateza@gmail.com>
2023-04-28 15:55:15 +0530

    stats for various kind of bookings

--- src/Component/Admin/AdminStats.js
+++ src/Component/Admin/AdminStats.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FileSaver from "file-saver";
 
 function AdminStats() {
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
   const [error, setError] = useState(null);
-  const [roomType, setRoomType] = useState("all");
-  const [location, setLocation] = useState("all");
+  const [roomType, setRoomType] = useState("seat");
+  const [location, setLocation] = useState(0);
+  const [locationVal, setLocationVal] = useState("all locations")
+  const [locations, setLocations] = useState(null);
   const handleFromDateChange = (event) => {
     setFromDate(event.target.value);
   };
@@ -19,17 +21,35 @@ function AdminStats() {
   const handleLocationChange = (event) => {
     setLocation(event.target.value);
   };
-  const handleRoomTypeChange = (event) => {
-    setRoomType(event.target.value);
-  };
-  const handleLocationChange = (event) => {
-    setLocation(event.target.value);
-  };
+  useEffect(
+    (e) => {
+      fetch("http://localhost:8081/api/location/", {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setLocations(data);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    },
+    []
+  );
   const handleDownloadPdf = () => {
     const header = "Bearer " + sessionStorage.getItem("accessToken");
     // const locationId = sessionStorage.getItem("userLocationId");
     fetch(
-      `http://localhost:8081/api/pdf/bookings/dateWise/${fromDate}/${toDate}`,
+      `http://localhost:8081/api/pdf/bookings/dateWise/${fromDate}/${toDate}/${location}/${roomType}`,
       {
         headers: {
           "Content-type": "application/json",
@@ -44,7 +64,7 @@ function AdminStats() {
         // Use FileSaver.js to save the blob as a file
         FileSaver.saveAs(
           blob,
-          `Bookings between ${fromDate} and ${toDate}.pdf`
+          `Bookings between ${fromDate} and ${toDate} at ${locationVal} for ${roomType}.pdf`
         );
       })
       .catch((error) => {
@@ -77,21 +97,19 @@ function AdminStats() {
 
         <label htmlFor="roomType">Select Type:</label>
         <select id="roomType" value={roomType} onChange={handleRoomTypeChange}>
-          <option value="regular-room">Seat</option>
-          <option value="board-room">Board Room</option>
-          <option value="conference-room">Conference Room</option>
-        </select>
+          <option value="seat">Seat</option>
+          <option value="board room">Board Room</option>
+          <option value="conference room">Conference Room</option>
+        </select> 
         <br />
 
         <label htmlFor="location">Location:</label>
         <select id="location" value={location} onChange={handleLocationChange}>
-          <option value="place">All</option>
+          <option value="0">All</option>
+          {locations && locations.map((location)=>{return(<option value={location.id}>{location.name}</option>)})}
         </select>
         <br />
 
-        <button className="ad" onClick={handleDownloadPdf}>
-          Download PDF
-        </button>
         <button className="ad" onClick={handleDownloadPdf}>
           Download PDF
         </button>
