d5b2744cf83d7dd52caf5def07bc3df95c717e56
nireeksha.jain <ss.jain@accolitedigital.com>
2023-05-03 11:12:03 +0530

    request from user

--- src/Component/Home/RequestAccess.js
+++ src/Component/Home/RequestAccess.js
@@ -1,61 +1,110 @@
 import React, { useEffect, useState } from "react";
 function RequestAccess({ onClose, ...props }) {
-  const [description, setDescription] = useState("");
+  // const [description, setDescription] = useState("");
+  const [bookingData, setBookingData] = useState({
+    date: "",
+    fromTime: "",
+    toTime: "",
+    roomType: "Available rooms",
+  });
   const email = sessionStorage.getItem("email");
   const token = "Bearer " + sessionStorage.getItem("accessToken");
 
   useEffect(() => {
-    if (description === "Your request has been sent succesfully!") {
+    if (bookingData.description === "Your request has been sent succesfully!") {
       window.location.reload();
     }
-  }, [description]);
+  }, [bookingData.description]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Submitting description: ${description}`);
-    const requestData = {
-      email: sessionStorage.getItem("email"),
-      description: description,
-    };
-    console.log(JSON.stringify(requestData));
+    console.log(`Submitting booking data: ${JSON.stringify(bookingData)}`);
     fetch("http://localhost:8081/api/requestBooking/", {
       method: "POST",
       headers: { "Content-Type": "application/json", Authorization: token },
-      body: JSON.stringify(requestData),
+      body: JSON.stringify({
+        email: email,
+        ...bookingData,
+      }),
     })
       .then((response) => {
         if (response.ok) {
-          setDescription("Your request has been sent successfully!");
+          setBookingData({
+            date: "",
+            fromTime: "",
+            toTime: "",
+            roomType: "",
+            description: "Your request has been sent successfully!",
+          });
           setTimeout(() => {
             props.setShowModal(false);
           }, 1000);
         } else {
-          throw new Error("failed to send request");
+          throw new Error("Failed to send request");
         }
       })
       .catch((error) => {
-        setDescription(error.Description);
+        setBookingData({
+          ...bookingData,
+          description: error.description,
+        });
       });
   };
 
-  return (
-    <div className="request-card">
-      <h3 >Request Board/Discussion Room</h3>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          className="form-control"
-          placeholder="Please provide your role along with date, time slot and purpose"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-        <span className="request-card-center">
-          <button type="submit">Request</button>
-          &nbsp;&nbsp;
-          <button onClick={onClose}>Close</button>
-        </span>
-      </form>
-    </div>
-  );
-}
 
-export default RequestAccess;
+  return (
+      <div className="request-card">
+        <h3>Request Board/Discussion Room</h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            value={bookingData.date}
+            onChange={(event) =>
+              setBookingData({ ...bookingData, date: event.target.value })
+            }
+          />
+  
+          <label htmlFor="fromTime">From</label>
+          <input
+            type="time"
+            id="fromTime"
+            value={bookingData.fromTime}
+            onChange={(event) =>
+              setBookingData({ ...bookingData, fromTime: event.target.value })
+            }
+          />
+  
+          <label htmlFor="toTime">To</label>
+          <input
+            type="time"
+            id="toTime"
+            value={bookingData.toTime}
+            onChange={(event) =>
+              setBookingData({ ...bookingData, toTime: event.target.value })
+            }
+          />
+  
+          <label htmlFor="roomType">Room Type</label>
+          <select
+            id="roomType"
+            value={bookingData.roomType}
+            onChange={(event) =>
+              setBookingData({ ...bookingData, roomType: event.target.value })
+            }
+          >
+            <option value="available">Available rooms</option>
+          </select> <br></br>
+  
+          <span className="request-card-center">
+            <button type="submit">Request</button>
+            &nbsp;&nbsp;
+            <button onClick={onClose}>Close</button>
+          </span>
+        </form>
+      </div>
+    );
+  }
+  
+  export default RequestAccess;
