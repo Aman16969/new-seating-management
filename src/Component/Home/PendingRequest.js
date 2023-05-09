e9da7059a11789cc8cb5f2aff4bd7ce994770205
nireeksha.jain <ss.jain@accolitedigital.com>
2023-05-09 10:12:59 +0530

    date changes

--- src/Component/Home/PendingRequest.js
+++ src/Component/Home/PendingRequest.js
@@ -57,7 +57,7 @@ const PendingRequest = ({rFlag, setRFlag, ...props}) => {
   function formatDate(dateString) {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
     const year = date.getFullYear().toString();
     return `${day}-${month}-${year}`;
   }
